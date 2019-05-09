484f892458060f6b03c4c9de1392e87298ce4eec
github username <github email address>
2019-05-08 19:22:19 -0700

    added new layout for mailboxes

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -10,7 +10,7 @@ class EmailsController < ApplicationController
 
       @contacts = @current_user.contacts
 
-      erb :'emails/index'
+      erb :'emails/index' , :layout => :mailbox
     end
   end
 
